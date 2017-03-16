c3e467ec3679e90695721a8852750fbf201f947d
terry chay <tychay@php.net>
2017-03-16 14:53:09 -0700

    Started a tutorial on learning React
    https://www.youtube.com/watch?v=-AbaV3nrw6E&list=PL6gx4Cwl9DGBuKtLgPR_zWYnrwv-JllpA (13 videos)
    
    Completed videos 1-7
    
    Signed-off-by: terry chay <tychay@php.net>

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,6 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var Bacon = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h2>Bacon component!</h2> 
+        <p>This is a simple component!</p>
+      </div>
+    );
+  }
+});
+
+var Movie = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.genre}</h2>
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  edit: function() {
+    alert('Editing comment');
+  },
+  remove: function() {
+    alert('Remove comment');
+  },
+  render: function() {
+    return(
+      <div className="commentContainer">
+        <div className="commentText">{this.props.children}</div>
+        <button onClick={this.edit} className="button-primary">Edit</button>
+        <button onClick={this.remove} className="button-danger">Remove</button>
+      </div>
+    )
+  }
+});
+
+var CheckBox = React.createClass({
+  getInitialState: function() {
+    return {checked: true};
+  },
+  handleChecked: function() {
+    this.setState({checked: !this.state.checked});
+  },
+  render: function() {
+    var msg = (this.state.checked) ? 'checked' : 'unchecked';
+    return(
+      <div className="commentContainer">
+        <input type="checkbox" onChange={this.handleChecked} defaultChecked={this.state.checked} />
+        <h3>Checkbox is {msg}</h3>
+      </div>
+    )
+  }
+});
+
+
 class App extends Component {
   render() {
     return (
@@ -10,9 +69,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <div className="board">
+          <Comment>Hey now</Comment>
+          <Comment>Beans</Comment>
+          <Comment>Tuna</Comment>
+        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Movie title="Avatar" genre="action" />
+          <Bacon />
         </p>
+        <CheckBox />
       </div>
     );
   }
